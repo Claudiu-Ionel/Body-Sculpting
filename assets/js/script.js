43e5e706023559111a4d9a8bcff17f668bc89033
Claudiu Ionel <ionel.claudiu394@gmail.com>
2023-09-20 13:10:29 +0200

    added body svg and click events for muscle group selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,40 @@
-console.log("script loaded");
+window.addEventListener("load", (event) => {
+  console.log("page is fully loaded");
+  // Get the Object by ID
+  const objectSVG = document.getElementById("body-svg");
+  objectSVG.style.opacity = "1";
+  // Get the SVG document inside the Object tag
+  const svgDoc = objectSVG.contentDocument;
+  // Get one of the SVG items by ID;
+  const svgGroups = svgDoc.getElementById("body-back").children;
+  let initialColor;
+  for (let bodyPart of svgGroups) {
+    if (bodyPart.id === "hands-fingers") continue;
+    bodyPart.addEventListener("mouseover", () => {
+      initialColor = bodyPart.style.color;
+      if (bodyPart.style.color === "orange") return;
+      bodyPart.style.color = "#3a3a3a";
+      bodyPart.style.opacity = "1";
+      bodyPart.style.cursor = "pointer";
+    });
+
+    bodyPart.addEventListener("mouseout", () => {
+      if (bodyPart.style.color === "orange") return;
+
+      bodyPart.style.color = initialColor;
+    });
+    bodyPart.addEventListener("click", () => {
+      bodyPart.style.color = "orange";
+
+      for (let group of svgGroups) {
+        if (group.id !== bodyPart.id) {
+          let colorChange = "#8a8a8a";
+          group.style.color = colorChange;
+          initialColor = colorChange;
+        } else {
+          group.style.color = "orange";
+        }
+      }
+    });
+  }
+});
