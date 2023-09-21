f59f8e648d67ce3dee19a2cd97e0818c43d7644b
Claudiu Ionel <ionel.claudiu394@gmail.com>
2023-09-21 12:10:20 +0200

    added exercise section functionality and created external data file

---
+++ assets/data/data.js
@@ -0,0 +1,21 @@
+export const exercises = {
+  "head-back": {
+    title: "Neck Muscles",
+  },
+  back: {
+    title: "Back muscles",
+  },
+  "shoulders-back": {
+    title: "Shoulders",
+  },
+  triceps: {
+    title: "Triceps",
+  },
+  "forearm-back": {
+    title: "Forearms",
+  },
+  butt: {
+    title: "Gluteus MAXIMUS",
+  },
+  "legs-lower-back": { title: "Calf muscles" },
+};
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+import { exercises } from "../data/data.js";
+
 window.addEventListener("load", () => {
   // Get the Object by ID
   const objectSVG = document.getElementById("body-svg-back");
@@ -28,19 +30,22 @@ function mouseOut(initialRColor, bodyP) {
 }
 
 function onClick(svgG, bodyP) {
+  // Body parts functionality
   for (let group of svgG) {
     if (group.id !== bodyP.id) {
       let colorChange = "#8a8a8a";
       group.style.color = colorChange;
-      initialColor = colorChange;
+      // initialColor = colorChange;
     } else {
       group.style.color = "orange";
     }
   }
+  addDataToExerciseSection(bodyP);
 }
 
 function attachEventListeners(initialCol, svgG) {
   for (let bodyPart of svgG) {
+    console.log(bodyPart.id);
     // add transition animation on body parts
     bodyPart.style.transition = "color 100ms ease-in";
 
@@ -59,4 +64,18 @@ function attachEventListeners(initialCol, svgG) {
       onClick(svgG, bodyPart);
     });
   }
+}
+
+function addDataToExerciseSection(bodyP) {
+  let bodyPartData = exercises[bodyP.id];
+  // Make exercise section visible
+  const exercisesSection = document.getElementById("exercises-wrapper");
+  exercisesSection.style.display = "flex";
+
+  // Add data to the section
+
+  let html = `
+  <h2>${bodyPartData.title}</h2>
+  `;
+  exercisesSection.innerHTML = html;
 }
